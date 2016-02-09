6135d798fdbf1a15a30cc30ff9cbb672b4e2e175
Matt G <matt.gaikema@tamu.edu>
2016-02-08 22:29:27 -0600

    Added features

--- AppGame/AppGame/MainWindow.xaml.cs
+++ AppGame/AppGame/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//using System.Drawing;
 
 //https://github.com/TexAgg/MyMP3/blob/master/MyMP3/Form1.cs
 
@@ -22,6 +23,9 @@ namespace AppGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool main_button_pressed = false;
+        private int count = 0;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -36,7 +40,27 @@ namespace AppGame
 
         private void main_button_click(object sender, RoutedEventArgs e)
         {
-            main_label.Content = "Yay";
+            count++;
+            main_button_pressed = !main_button_pressed;
+            main_label.Content = labeler();
+            strobe();
+            display_count.Text = count.ToString();
+        }
+
+        private string labeler()
+        {
+            if (main_button_pressed)
+                return "yay!";
+            else
+                return "Press me!";
+        }
+
+        private void strobe()
+        {
+            if (main_button_pressed)
+                background.Background = new SolidColorBrush(Color.FromRgb(255, 204, 0));
+            else
+                background.Background = new SolidColorBrush(Color.FromRgb(128,128,128));
         }
     }
 }
